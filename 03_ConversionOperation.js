13fcc5ef996ec7f790f9d32a255ca180040d4601
Mayur-Atakari <147389063+Mayur-Atakari@users.noreply.github.com>
2024-05-18 08:19:20 +0000

    conversion of datatypes

---
+++ 03_ConversionOperation.js
@@ -0,0 +1,32 @@
+let score = "mayur"
+
+//console.log(typeof score);
+
+let ValueInNumber = Number(score)
+//console.log(ValueInNumber)
+
+
+// "33" => 33
+// "33abd" => NaN
+// true => 1 // false => 0
+// null => 0
+// undefined => NaN
+
+let isLoggedIn = "mayur"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+//console.log(booleanIsLoggedIn)
+
+
+// 1 => true
+// 2 => false
+// "" => false
+// mayur => true
+
+
+let someNumber = ""
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
