a17a1ade4d2070285322e2aedfc4348e59972d80
Mayur-Atakari <147389063+Mayur-Atakari@users.noreply.github.com>
2024-05-20 07:47:37 +0000

    conversion

--- 03_ConversionOperation.js
+++ 03_ConversionOperation.js
@@ -28,5 +28,38 @@ let someNumber = ""
 
 let stringNumber = String(someNumber)
 
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+let value = 4
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+// console.log(2**2);
+
+
+let str1 = "hello"
+let str2 = "mayur"
+
+let str3 = str1 +" "+ str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+
+// console.log(+"");
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
+
